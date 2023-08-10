d704be4282f5dae3b08318dbc7eb3eea048b1b46
AhmedAbdelAziim <ahmed.abdelaziim1@gmail.com>
2023-08-10 15:58:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing this value
+ * return : will be 0
+ */
+int main(void)
+{	char mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts(mystr);
+	return (0);
+}
