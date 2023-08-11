83355442cc10f4fa521272c27853210991859444
AhmedAbdelAziim <ahmed.abdelaziim1@gmail.com>
2023-08-10 19:10:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
-#include <sys/syscall.h>
+#include <stdio.h>
 /**
  * main - entry point
  * Discription: 'printing without using printf or puts'
  * Return: 1 (success)
  */
 int main(void)
-{	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, 1, msg, sizeof(msg));
+{ write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
