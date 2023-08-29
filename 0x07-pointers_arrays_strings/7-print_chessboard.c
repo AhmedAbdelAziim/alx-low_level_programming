941a30d02cf40b74bc631785b45ec90945ac3b57
AhmedAbdelAziim <ahmed.abdelaziim1@gmail.com>
2023-08-28 18:14:32 -0700

    ./7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function that prints the chessboard.
+ * @a: array for raws
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, e;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (e = 0; e < 8; e++)
+		{
+			_putchar(a[i][e]);
+		}
+		_putchar('\n');
+	}
+}
