12f2fbfe3ae5606059a614c8ef58d8e56308065b
AhmedAbdelAziim <ahmed.abdelaziim1@gmail.com>
2023-09-01 13:00:07 -0700

    ./nargs hello, world

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguments passed into it.
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)*argv[0];
+	printf("%d\n", argc - 1);
+	return (0);
+}
