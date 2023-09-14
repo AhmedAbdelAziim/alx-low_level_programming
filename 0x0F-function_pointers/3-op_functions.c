2633a0bc833bfed81b0b072e64b06d4ee200f461
AhmedAbdelAziim <ahmed.abdelaziim1@gmail.com>
2023-09-13 17:11:54 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - sum
+ * @a: 1st input
+ * @b: 2nd input
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - difference
+ * @a: 1st input
+ * @b: 2nd input
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - product
+ * @a: 1st input
+ * @b: 2nd input
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: 1st input
+ * @b: 2nd input
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - division 
+ * @a: 1st input
+ * @b: 2nd input
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
