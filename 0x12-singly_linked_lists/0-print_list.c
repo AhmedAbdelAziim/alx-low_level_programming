a1524034b760caea267e7a683dcf7af8825f5fc4
AhmedAbdelAziim <ahmedabdelaziim1@gmail.com>
2023-09-21 12:05:43 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements
+ * @h: pointer
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
